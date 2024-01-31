49908923b0eb1f2eb12e147557797d710aa71758
npearsonat <134087337+npearsonat@users.noreply.github.com>
2024-01-30 20:06:04 -0800

    Add files via upload

---
+++ Nathaniel_ArrayOfSalariesApp/Nathaniel_ArrayOfSalariesApp/Nathaniel_ArrayOfSalariesApp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using static System.Console;
+
+namespace SalariesApp
+{
+    class SalariesApp
+    {
+        // Main method. I separated it from the rest RunProgram method so the user can repeat the whole thing multiple times, as the directions say. 
+        // The user is prompted after RunProgram finishes to input the right character if they wish to continue
+        // I used ReadLine() for char because it was messing up the other ReadLines later on if I used Read().  
+        static void Main()
+        {
+            char pEnd = 'c';
+            while (pEnd == 'c') 
+            {
+                RunProgram();
+                pEnd = Convert.ToChar(Console.ReadLine());
+            }
+
+        }
+
+        public static void RunProgram()
+        {
+            //Creates new Salaries Array
+
+            Salaries monthlySalaries = new Salaries();
+
+            WriteLine("Input Monthly Salaries");
+            WriteLine("**********************\n");
+            WriteLine("Enter Values for monthly salaries between 3000-5000");
+
+            // This for loop is designed so the user must enter a number between 3000 and 5000 or else they will be prompted to re-enter their number.
+            // The first part is for entering each month
+            for (int i = 0; i < 12; i++)
+            {
+
+                int month = i + 1;
+                Write("Enter salary for month {0}: ", month);
+                double value = Convert.ToDouble(Console.ReadLine());
+                //If the value is good then it will assign it to the array index
+                if (value > 2999 && value < 5001)
+                {
+                    monthlySalaries.months[i] = value;
+                }
+                // If the value is bad then it will keep asking for one until it is between 3000 and 5000
+                else
+                {
+                    while (value < 3000 || value > 5000)
+                    {
+                        WriteLine("Improper Value Entered. Please Enter Value from 3000-5000");
+                        value = Convert.ToDouble(Console.ReadLine());
+                        if (value > 2999 && value < 5001)
+                        {
+                            monthlySalaries.months[i] = value;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //This part just uses WriteLine to show the results for various methods
+
+            WriteLine("\nSalary Statistics\n*****************\n");
+            WriteLine("Total Annual Salary\t\t\t\t{0:C}", monthlySalaries.ComputeTotalSalary());
+            WriteLine("Average Salary\t\t\t\t\t{0:C}", monthlySalaries.ComputeAverageSalary());
+            WriteLine("Lowest Monthly Salary in {0}:\t\t{1:C}", monthlySalaries.GetLowestMonthName(), monthlySalaries.ComputeLowestSalary());
+            WriteLine("Highest Monthly Salary in {0}:\t\t{1:C}", monthlySalaries.GetHighestMonthName(), monthlySalaries.ComputeHighestSalary());
+            WriteLine("\nNumber of Months Below Average ({0:C}):\t{1}", monthlySalaries.ComputeAverageSalary(), monthlySalaries.MonthsBelowAverageSalary());
+            WriteLine("\nSalary By Quarter\n************\nValues in the Salary Array\n");
+
+            // ToString method to show the values in our array
+            string quarters = monthlySalaries.ToString();
+            WriteLine(quarters);
+
+            WriteLine("\nSalaries ranged from {0} to {1}", monthlySalaries.ComputeLowestSalary(), monthlySalaries.ComputeHighestSalary());
+            // Reminds user to enter char if they want to continue
+            WriteLine("\nPress c to continue or e to end the program");
+        
+        }
+       }
+    }
---
+++ Nathaniel_ArrayOfSalariesApp/Nathaniel_ArrayOfSalariesApp/Salaries.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace SalariesApp
+{
+     public class Salaries
+    {
+        // Salary Array Initatior
+        public double[] months = new double[12];
+
+        //Computes total salary for all months
+        public double ComputeTotalSalary() 
+        {
+            double sum = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                sum += months[i];
+            }
+            return sum;
+               
+        }
+        //Computes lowest salary value
+        public double ComputeLowestSalary() 
+        {
+            return months.Min();
+        }
+        //Computes highest salry value
+        public double ComputeHighestSalary() 
+        {
+            return months.Max();
+        }
+        //Computes average salary
+        public double ComputeAverageSalary()
+        {
+            return months.Average();
+        }
+        //Computes average salary minues the lowest month value. Was not used in main method
+        public double ComputeAverageSalaryMinus1()
+        {
+            return (months.Sum() - months.Min())/(months.Length-1);
+        }
+
+        //Computes the number of months in the year prior to the lowest salaried month
+        public int MonthsBelowAverageSalary() 
+        {
+            int counter = 0;
+            foreach (int i in months)
+            {
+                if (i < months.Average())
+                {
+                    counter++;
+                }
+            }
+            return counter;
+
+        }
+
+        //Gets the month name for the lowest salary month
+        public string GetLowestMonthName() 
+        {
+            string MinMonth = "";
+
+            switch (Array.IndexOf(months, months.Min()))
+            {
+                case 0: MinMonth = "January"; break;
+                case 1: MinMonth = "February"; break;
+                case 2: MinMonth = "March"; break;
+                case 3: MinMonth = "April"; break;
+                case 4: MinMonth = "May"; break;
+                case 5: MinMonth = "June"; break;
+                case 6: MinMonth = "July"; break;
+                case 7: MinMonth = "August"; break;
+                case 8: MinMonth = "September"; break;
+                case 9: MinMonth = "October"; break;
+                case 10: MinMonth = "November"; break;
+                case 11: MinMonth = "December"; break;
+            }
+            return MinMonth;
+        }
+
+        //Gets the month name for the highest salary month
+        public string GetHighestMonthName()
+        {
+            string MaxMonth = "";
+
+            switch (Array.IndexOf(months, months.Max()))
+            {
+                case 0: MaxMonth = "January"; break;
+                case 1: MaxMonth = "February"; break;
+                case 2: MaxMonth = "March"; break;
+                case 3: MaxMonth = "April"; break;
+                case 4: MaxMonth = "May"; break;
+                case 5: MaxMonth = "June"; break;
+                case 6: MaxMonth = "July"; break;
+                case 7: MaxMonth = "August"; break;
+                case 8: MaxMonth = "September"; break;
+                case 9: MaxMonth = "October"; break;
+                case 10: MaxMonth = "November"; break;
+                case 11: MaxMonth = "December"; break;
+            }
+            return MaxMonth;
+        }
+
+        //Converts the moth array to a string and puts it in a 3x4 grid
+
+        public override string ToString()
+        {
+            return $"Qtr 1: \t {months[0]} \t {months[1]} \t { months[2]} \n" +
+                   $"Qtr 2: \t {months[3]} \t {months[4]} \t {months[5]} \n" +
+                   $"Qtr 3: \t {months[6]} \t {months[7]} \t {months[8]} \n" +
+                   $"Qtr 4: \t {months[9]} \t {months[10]} \t {months[11]}";
+        }
+
+
+    }
+}
